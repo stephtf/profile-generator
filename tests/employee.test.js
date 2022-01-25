0ad1959f6583ff4f1fff55abf98732ecd3eecacc
Stephanie Fajardo <stfajardo@gmail.com>
2022-01-25 09:52:46 -0500

    updated my index.js to add a first question function

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,11 +1,11 @@
 const Employee = require('../lib/employee');
 
 describe('function for the employee data', () => {
-    const employee = new Employee('Steph,' 222, 'stephfajardo@gmail.com'); 
+    const employee = new Employee(); 
 
-    expect(employee.name).toEqual(String); 
-    expect(employee.id).toEqual(Number); 
-    expect(employee.email).toEqual(String); 
+    expect(employee.name).toEqual(expect.any(String)); 
+    expect(employee.id).toEqual(expect.any(Number)); 
+    expect(employee.email).toEqual(expect.any(String)); 
 
 // get role function 
     expect(employee.getRole()).toEqual("Employee");
