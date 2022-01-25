bf4857c10b4116bc86ffe695129eeb847c1e0605
Stephanie Fajardo <stfajardo@gmail.com>
2022-01-25 17:02:52 -0500

    got the questions to prompt

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -3,11 +3,11 @@ const Employee = require('../lib/employee');
 describe('function for the employee data', () => {
     const employee = new Employee(); 
 
-    expect(employee.name).toEqual(expect.any(String)); 
-    expect(employee.id).toEqual(expect.any(Number)); 
-    expect(employee.email).toEqual(expect.any(String)); 
+    expect(data.name).toEqual(expect.any(String)); 
+    expect(data.id).toEqual(expect.any(Number)); 
+    expect(data.email).toEqual(expect.any(String)); 
 
 // get role function 
-    expect(employee.getRole()).toEqual("Employee");
+    // expect(data.employee.toEqual("manager", "engineer", "intern");
 
 });
